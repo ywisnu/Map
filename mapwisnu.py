44538e4b1a42efad5f9a7a3f2d29db8a21a0e7c7
ywisnu <starbox8888@gmail.com>
2024-07-15 11:27:58 +0700

    Update mapwisnu.py

--- mapwisnu.py
+++ mapwisnu.py
@@ -1,31 +1,35 @@
 import folium
 import streamlit as st
 from folium.plugins import GroupedLayerControl
-from folium.plugins import Draw
+
 from streamlit_folium import st_folium
 
 st.set_page_config(
-    page_title="Fiber Optic Project",
-    page_icon=":earth_asia:",
+    page_title="streamlit-folium documentation: Grouped Layer Control",
+    page_icon=":pencil:",
     layout="wide",
 )
 
-    m = folium.Map(location=[-6.211156,106.816281], zoom_start=13,tiles="Cartodb Positron")
-    Draw(export=True).add_to(m)
-    
-    url = "https://raw.githubusercontent.com/ywisnu/Map/main/Data/merged.json"
-    folium.GeoJson(url).add_to(m)
+st.title("streamlit-folium: Grouped Layer Control")
 
-    folium.Marker(
-    location=[-6.230830556,106.7775056],
-    popup=folium.Popup("PAG KEBAYORAN LAMA JAW-JK-KYB-1231", parse_html=True, max_width=100),
-    ).add_to(m)
 
-    folium.Marker(
-    location=[-6.244827778,106.8261472],
-    popup=folium.Popup("HUT Mampang JAW-JK-KYB-1692", parse_html=True, max_width=100),
-    ).add_to(m)
+m = folium.Map([40.0, 70.0], zoom_start=6)
+
+fg1 = folium.FeatureGroup(name="g1")
+fg2 = folium.FeatureGroup(name="g2")
+fg3 = folium.FeatureGroup(name="g3")
+folium.Marker([40, 74]).add_to(fg1)
+folium.Marker([38, 72]).add_to(fg2)
+folium.Marker([40, 72]).add_to(fg3)
+m.add_child(fg1)
+m.add_child(fg2)
+m.add_child(fg3)
+
+folium.LayerControl(collapsed=False).add_to(m)
+
+GroupedLayerControl(
+    groups={"groups1": [fg1, fg2]},
+    collapsed=False,
+).add_to(m)
 
 st_folium(m)
-
-        
