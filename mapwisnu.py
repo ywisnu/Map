9e6724d34d6e0a99cd13a79624745269468ec411
ywisnu <starbox8888@gmail.com>
2024-07-20 23:20:35 +0700

    Update mapwisnu.py

--- mapwisnu.py
+++ mapwisnu.py
@@ -30,6 +30,11 @@ m.add_child(G2)
 
 folium.LayerControl(collapsed=False).add_to(m)
 
+  folium.Marker(
+    location=[-6.230830556,106.7775056],
+    popup=folium.Popup("PAG KEBAYORAN LAMA JAW-JK-KYB-1231", parse_html=True, max_width=100),
+    ).add_to(m)
+
 col1, col2 = st.columns(2)
 with col1:
     st.markdown("## XL Project")
