bd5e20cb08f850951c4fe3bd9c24074b32bffff6
ywisnu <starbox8888@gmail.com>
2024-07-20 23:21:06 +0700

    Update mapwisnu.py

--- mapwisnu.py
+++ mapwisnu.py
@@ -30,8 +30,7 @@ m.add_child(G2)
 
 folium.LayerControl(collapsed=False).add_to(m)
 
-  folium.Marker(
-    location=[-6.230830556,106.7775056],
+folium.Marker(location=[-6.230830556,106.7775056],
     popup=folium.Popup("PAG KEBAYORAN LAMA JAW-JK-KYB-1231", parse_html=True, max_width=100),
     ).add_to(m)
 
