413895da70461b640e42b869f2545c1c8d5f6b09
ywisnu <starbox8888@gmail.com>
2024-07-20 18:40:17 +0700

    Update mapwisnu.py

--- mapwisnu.py
+++ mapwisnu.py
@@ -19,15 +19,14 @@ st.set_page_config(
 st.header('Fiber Optic Project')
 
 
-    m= folium.Map(location=[-6.211156,106.816281], zoom_start=13,tiles="Cartodb Positron")
-    url= "https://raw.githubusercontent.com/ywisnu/Map/main/Data/merged.json"
-    url2 = "https://raw.githubusercontent.com/ywisnu/Map/main/Data/RouteR5.geojson"
-    folium.GeoJson(url).add_to(m)
+m= folium.Map(location=[-6.211156,106.816281], zoom_start=13,tiles="Cartodb Positron")
+url= "https://raw.githubusercontent.com/ywisnu/Map/main/Data/merged.json"
+url2= "https://raw.githubusercontent.com/ywisnu/Map/main/Data/RouteR5.geojson"
+folium.GeoJson(url).add_to(m)
 
+G2 = folium.GeoJson(url2).add_to(m)
 
-    G2 = folium.GeoJson(url2).add_to(m)
-
-    m.add_child(G2)
+m.add_child(G2)
 
 folium.LayerControl(collapsed=False).add_to(m)
 
